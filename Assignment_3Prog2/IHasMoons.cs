614ee230f12e9c750a59b282d3a937f9832bf92d
Das Karamjit <dkaramji@my.centennialcollege.ca>
2020-07-24 10:49:30 -0700

    IhasMoons interface created

---
+++ Assignment_3Prog2/IHasMoons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3Prog2
+{
+    interface IHasMoons
+    {
+         bool HasMoons();
+
+    }
+}
