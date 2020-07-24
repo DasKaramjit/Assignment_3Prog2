46436ec5f38d76d2b250d70d0fb42f210f65fe8b
Das Karamjit <dkaramji@my.centennialcollege.ca>
2020-07-24 11:09:09 -0700

    Program.cs edited

--- Assignment_3Prog2/Program.cs
+++ Assignment_3Prog2/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Assignment_3Prog2
 {
+
+    /* Name : Das Karamjit
+     * Student ID : 301108505
+     * Date Last Modified : 24-7-2020
+     * Program Description : This is the program based on abstract classes and interface where Planet is abstract class
+     * and it has 2 subclasses, 3 interfaces and 1 program.cs file.
+     * Revision History : Program is  revised several times to check for errors.
+     */
     class Program
     {
         static void Main(string[] args)
